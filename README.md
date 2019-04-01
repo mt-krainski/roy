be7b5c977f8294a5e57b9b7499db215f9020a0b2
Mateusz Krainski <mateusz.krainski@showmax.com>
2019-04-01 22:36:45 +0200

    Minor refactor of admin page

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 This is a personal information and data management system with elements of automation. Hopefully, eventually, most of it will be automated. 
 
 Roy is just a random name I came up with, it's not yet an acronym and doesn't have any other specific meaning. Any resemblance to real-life or fictional entities is purely coincidental.
+
+### Feature ideas
+- Location logging,
+- Markdown based notebook,
+- 
--- activity_logger/admin.py
+++ activity_logger/admin.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.contrib.gis.admin import OSMGeoAdmin
@@ -6,23 +8,43 @@ from .models import Establishment, ActivityType, Activity
 
 @admin.register(Establishment)
 class EstablishmentAdmin(OSMGeoAdmin):
-    list_display = ('name', 'location', 'updated_at')
+    list_display = ('name', 'location', 'updated_at', '_user')
+    fields = ('name', 'location', 'user')
+
+    def _user(self, obj):
+        return obj.user.username if obj.user else '--'
 
 
 @admin.register(ActivityType)
 class ActivityTypeAdmin(ModelAdmin):
-    list_display = ['name', 'updated_at']
+    list_display = ('name', 'updated_at')
 
 
 @admin.register(Activity)
 class ActivityAdmin(ModelAdmin):
-    list_display = [
-        'uuid',
+    list_display = (
+        '_uuid_short',
+        '_user',
         'name',
         '_activity_type',
         '_establishment',
         'updated_at',
-    ]
+        'start_time',
+        'end_time',
+        '_duration'
+    )
+
+    list_filter = (
+        'activity_type',
+        'user'
+    )
+
+    def _uuid_short(self, obj):
+        return str(obj.uuid).split('-')[0]
+    _uuid_short.short_description = 'UUID'
+
+    def _user(self, obj):
+        return obj.user.username if obj.user else '--'
 
     def _activity_type(self, obj):
         return obj.activity_type.name
@@ -30,3 +52,9 @@ class ActivityAdmin(ModelAdmin):
     def _establishment(self, obj):
         return obj.establishment.name
 
+    def _duration(self, obj):
+        if obj.start_time is not None and obj.end_time is not None:
+            duration = obj.end_time - obj.start_time
+            return duration - timedelta(microseconds=duration.microseconds)
+
+        return '--'
