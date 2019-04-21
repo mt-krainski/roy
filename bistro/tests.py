1a1e6c1d62e59750068c310e8747d537ff349dac
Mateusz Krainski <mateusz.krainski@showmax.com>
2019-04-21 20:08:08 +0200

    Add unit tests for bistro views

--- bistro/tests.py
+++ bistro/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.contrib.gis.geos import Point
 from django.test import TestCase
+from django.urls import reverse
 
+from bistro.forms import PlaceForm
 from bistro.models import BistroType, ConsumableType, BistroPlace, Visit
 
 
@@ -14,16 +16,26 @@ class BistroTestCase(TestCase):
         self.consumable_type, _ = ConsumableType.objects.get_or_create(
             name='Test consumable'
         )
-        self.test_user, _ = User.objects.get_or_create(
+        self.test_user = User.objects.create_user(
             username='test',
             password='test',
         )
+        self.test_user.save()
         self.bistro_place, _ = BistroPlace.objects.get_or_create(
             name='Test place',
             location=Point(0, 0),
             user=self.test_user,
             type=self.bistro_type,
         )
+
+        for i in range(5):
+            BistroPlace.objects.get_or_create(
+                name=f'Test place {i}',
+                location=Point(0, 0),
+                user=self.test_user,
+                type=self.bistro_type,
+            )
+
         self.visit, _ = Visit.objects.get_or_create(
             place=self.bistro_place,
             user=self.test_user,
@@ -44,3 +56,61 @@ class BistroTestCase(TestCase):
         self.assertEqual(
             str(self.visit.consumable), 'Test consumable')
 
+    def test_url_exists(self):
+        self.client.login(
+            username='test',
+            password='test',
+        )
+        response = self.client.get(reverse('bistro:random_bistro'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.context['user']), 'test')
+        self.assertTemplateUsed(
+            response, 'bistro/bistro.html')
+        response = self.client.get(reverse('bistro:add_place'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(response.context['user']), 'test')
+        self.assertTemplateUsed(response, 'bistro/add.html')
+
+    def test_random_bistro(self):
+        self.client.login(
+            username='test',
+            password='test',
+        )
+        found_bistros = []
+        for i in range(100):
+            response = self.client.get(reverse('bistro:random_bistro'))
+            found_bistros.append(response.context['bistro'])
+
+        self.assertEqual(
+            set(found_bistros),
+            set(BistroPlace.objects.filter(user=self.test_user))
+        )
+
+    def test_add_place_form(self):
+        self.client.login(
+            username='test',
+            password='test',
+        )
+        response = self.client.get(reverse('bistro:add_place'))
+        self.assertIsInstance(
+            response.context['form'], PlaceForm
+        )
+
+    def test_add_place(self):
+        self.client.login(
+            username='test',
+            password='test',
+        )
+        response = self.client.post(
+            reverse('bistro:add_place'),
+            {
+                'place_name': 'New Test Place',
+                'type': self.bistro_type.pk,
+                'location': str(Point(0, 0)),
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('bistro:add_place'))
+
+        new_place = BistroPlace.objects.filter(name='New Test Place')
+        self.assertTrue(new_place.exists())
--- bistro/views.py
+++ bistro/views.py
@@ -1,8 +1,10 @@
 from random import randint
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http.response import HttpResponseBadRequest
+from django.http.response import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils.text import slugify
 
 from bistro.forms import PlaceForm
@@ -41,8 +43,13 @@ def add_place(request):
             )
             new_place.save()
 
-            return render(
-                request, 'bistro/added.html', {'bistro_name': new_place.name})
+            messages.add_message(
+                request,
+                messages.INFO,
+                f'{new_place.name.capitalize()} has been added!')
+
+            return HttpResponseRedirect(
+                reverse('bistro:add_place'))
 
         return HttpResponseBadRequest
 
