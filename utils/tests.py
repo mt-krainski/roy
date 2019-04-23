bbf75c385cf851d9e7d825517a59ce6a4e12f9c3
Mateusz Krainski <mateusz.krainski@showmax.com>
2019-04-23 19:27:27 +0200

    Add test for health ciew

--- utils/tests.py
+++ utils/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from utils.views import health
+
+
+class UtilTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_health_view(self):
+        request = self.factory.get(reverse('utils:health'))
+        response = health(request)
+        self.assertEqual(response.status_code, 200)
